612b76ae1fabb412d324d6e70969c7a0f4418486
hari-19 <hari19@outlook.in>
2023-04-04 21:19:30 +0530

    Update get file function

--- client/main.go
+++ client/main.go
@@ -5,10 +5,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -19,18 +20,48 @@ import (
 type Client struct {
 	// Must embed an Inode for the struct to work as a node.
 	fs.Inode
-	id   int
-	name string
+	file File
+}
+
+const (
+	FILE   = false
+	FOLDER = true
+)
+
+type File struct {
+	name     string
+	id       int
+	fileType bool
 }
 
 // A temporary function that sends list of entries in a directory
-func getDir(id int) []int {
-	fmt.Println("From GetDir ", id)
-	out := []int{}
-	for i := 0; i < id; i++ {
-		out = append(out, i)
+func getDir() []File {
+	files := []File{
+		{name: "/root/abc.txt", id: 1, fileType: FILE},
+		{name: "/root/bcd.txt", id: 2, fileType: FILE},
+		{name: "/root/def.txt", id: 3, fileType: FILE},
+		{name: "/root/efg.txt", id: 4, fileType: FILE},
+		{name: "/root/fgh.txt", id: 5, fileType: FILE},
 	}
-	return out
+
+	return files
+}
+
+func getFile(name string) (File, error) {
+	files := []File{
+		{name: "/root/abc.txt", id: 1, fileType: FILE},
+		{name: "/root/bcd.txt", id: 2, fileType: FILE},
+		{name: "/root/def.txt", id: 3, fileType: FILE},
+		{name: "/root/efg.txt", id: 4, fileType: FILE},
+		{name: "/root/fgh.txt", id: 5, fileType: FILE},
+	}
+
+	for i := range files {
+		if files[i].name == name {
+			return files[i], nil
+		}
+	}
+	return File{}, errors.New("File not found")
 }
 
 // Mode for reference
@@ -45,10 +76,12 @@ func (n *Client) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
 	r := make([]fuse.DirEntry, 0)
 
 	// files := []string{"abc", "bcd", "def", "efg", "fgh"}
-	for _, val := range getDir(n.id) {
+	for _, val := range getDir() {
+		slices := strings.Split(val.name, "/")
+		name := slices[len(slices)-1]
 		d := fuse.DirEntry{
-			Name: strconv.Itoa(val),
-			Ino:  uint64(val), // Should be id of the file/ directory
+			Name: name,
+			Ino:  uint64(val.id), // Should be id of the file/ directory
 			Mode: fuse.S_IFREG,
 		}
 		r = append(r, d)
@@ -61,21 +94,28 @@ var _ = (fs.NodeLookuper)((*Client)(nil))
 
 // Lookup is part of the NodeLookuper interface“
 func (n *Client) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
-	i, err := strconv.Atoi(name)
+	fmt.Println("Lookup:", n.file.name)
+	fmt.Println("Name:", name)
+	fmt.Println("Context:", ctx)
+	file, err := getFile("/" + n.file.name + "/" + name)
+
+	fmt.Println("File:", file)
+	fmt.Println(err)
+
 	if err != nil {
 		return nil, syscall.ENOENT
 	}
 
+	fmt.Println("File ID:", file.id)
 	stable := fs.StableAttr{
 		Mode: fuse.S_IFREG,
 		// The child inode is identified by its Inode number.
 		// If multiple concurrent lookups try to find the same
 		// inode, they are deduplicated on this key.
-		Ino: uint64(i),
+		Ino: uint64(int(file.id)),
 	}
 
-	files := []string{"abc", "bcd", "def", "efg", "fgh"}
-	operations := &Client{id: i, name: files[i] + ".txt"}
+	operations := &Client{file: File{name: file.name, id: file.id, fileType: file.fileType}}
 
 	// The NewInode call wraps the `operations` object into an Inode.
 	child := n.NewInode(ctx, operations, stable)
@@ -88,7 +128,7 @@ func (n *Client) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*
 var _ = (fs.NodeOpener)((*Client)(nil))
 
 func (n *Client) Open(ctx context.Context, openFlags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
-	fd, err := os.Open("/home/hari/Documents/Projects/student-file-system/files/" + n.name)
+	fd, err := os.Open("/home/hari/Documents/Projects/student-file-system/files/" + n.file.name)
 	syscall.Dup(int(fd.Fd()))
 	if err != nil {
 		log.Fatal("Error Opening File")
@@ -116,7 +156,7 @@ func main() {
 	// This is where we'll mount the FS
 	mntDir := "/tmp/z"
 	os.Mkdir(mntDir, 0755)
-	root := &Client{id: 5}
+	root := &Client{file: File{name: "root", id: 0, fileType: FOLDER}}
 	server, err := fs.Mount(mntDir, root, &fs.Options{
 		MountOptions: fuse.MountOptions{
 			// Set to true to see how the file system works.
