9221470a26a46f2e1a00d516db2a2a851eb1b555
hari-19 <hari19@outlook.in>
2023-04-15 11:31:07 +0530

    Added some comments

--- client/main.go
+++ client/main.go
@@ -18,18 +18,20 @@ import (
 	"github.com/hanwen/go-fuse/v2/fuse"
 )
 
-type Client struct {
-	// Must embed an Inode for the struct to work as a node.
-	fs.Inode
-	mu      sync.Mutex
-	file    File
-	content []byte
-}
-
+// Constants that define the file type
 const (
 	FILE   = false
 	FOLDER = true
 )
+
+// File System Node Structure
+type FSNode struct {
+	// Must embed an Inode for the struct to work as a node.
+	fs.Inode
+	mu      sync.Mutex
+	file    File
+	content []byte // for testing - remove later
+}
 
 type File struct {
 	name     string
@@ -38,6 +40,7 @@ type File struct {
 	sz       uint64
 }
 
+// Temporary initial files used for testing
 var files = []File{
 	{name: "/root/abc.txt", id: 1, fileType: FILE},
 	{name: "/root/bcd.txt", id: 2, fileType: FILE},
@@ -51,6 +54,7 @@ func getDir() []File {
 	return files
 }
 
+// A temporary function that sends the file with the given name
 func getFile(name string) (File, error) {
 	for i := range files {
 		if files[i].name == name {
@@ -60,18 +64,18 @@ func getFile(name string) (File, error) {
 	return File{}, errors.New("File not found")
 }
 
-// Mode for reference
+// FUSE File Mode for reference
 // fuse.S_IFREG = File
 // fuse.S_IFDIR = directories
 
-// Ensure we are implementing the NodeReaddirer interface
-var _ = (fs.NodeReaddirer)((*Client)(nil))
+// For ReadDir
+var _ = (fs.NodeReaddirer)((*FSNode)(nil))
 
 // Readdir is part of the NodeReaddirer interface
-func (n *Client) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
+// It returns a list of files in the directory
+func (n *FSNode) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
 	r := make([]fuse.DirEntry, 0)
 
-	// files := []string{"abc", "bcd", "def", "efg", "fgh"}
 	for _, val := range getDir() {
 		var mode uint32
 		if val.fileType == FOLDER {
@@ -91,16 +95,14 @@ func (n *Client) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
 	return fs.NewListDirStream(r), 0
 }
 
-// Ensure we are implementing the NodeLookuper interface
-var _ = (fs.NodeLookuper)((*Client)(nil))
+// For Lookup
+var _ = (fs.NodeLookuper)((*FSNode)(nil))
 
-// Lookup is part of the NodeLookuper interface“
-func (n *Client) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+// Lookup is part of the NodeLookuper interface
+// It returns the file with the given name
+func (n *FSNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
 	// Get the file from the server
 	file, err := getFile(n.file.name + "/" + name)
-
-	fmt.Println("File:", file)
-	fmt.Println(err)
 
 	if err != nil {
 		return nil, syscall.ENOENT
@@ -119,7 +121,8 @@ func (n *Client) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*
 		Ino: uint64(int(file.id)),
 	}
 
-	operations := &Client{file: File{name: file.name, id: file.id, fileType: file.fileType, sz: n.file.sz}}
+	// Create a new FSNode for the file
+	operations := &FSNode{file: File{name: file.name, id: file.id, fileType: file.fileType, sz: n.file.sz}}
 
 	// The NewInode call wraps the `operations` object into an Inode.
 	child := n.NewInode(ctx, operations, stable)
@@ -129,39 +132,40 @@ func (n *Client) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*
 	return child, 0
 }
 
-var _ = (fs.NodeOpener)((*Client)(nil))
+var _ = (fs.NodeOpener)((*FSNode)(nil))
 
-func (n *Client) Open(ctx context.Context, openFlags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
-	// download file from server
+func (n *FSNode) Open(ctx context.Context, openFlags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+	// The commented code returns the file descriptor of the file
+
 	// fd, err := os.Open("/home/hari/Documents/Projects/student-file-system/files/" + n.file.name)
 	// syscall.Dup(int(fd.Fd()))
 	// if err != nil {
 	// 	log.Fatal("Error Opening File")
 	// }
 	// // fh = fs.NewLoopbackFile(int(fd.Fd()))
+
+	// Return nil as file handle to indicate that we don't need one.
 	return nil, 0, 0
 }
 
 // Implement handleless read.
-var _ = (fs.NodeReader)((*Client)(nil))
+// For Read
+var _ = (fs.NodeReader)((*FSNode)(nil))
 
-func (bn *Client) Read(ctx context.Context, fh fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+// Read is part of the NodeReader interface.
+// It returns the content of the file as a byte array to the client.
+// Offset defines the starting point of the read.
+func (bn *FSNode) Read(ctx context.Context, fh fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
 	bn.mu.Lock()
 	defer bn.mu.Unlock()
 
-	// end := off + int64(len(dest))
-	// if end > int64(len(bn.content)) {
-	// 	end = int64(len(bn.content))
-	// }
-
-	// We could copy to the `dest` buffer, but since we have a
-	// []byte already, return that.
 	copy(dest, bn.content[off:])
-	// return , 0
+
 	return fuse.ReadResultData(dest), 0
 }
 
-func (bn *Client) resize(sz uint64) {
+// To Resize the temp content buffer in FSNode
+func (bn *FSNode) resize(sz uint64) {
 	if sz > uint64(cap(bn.content)) {
 		n := make([]byte, sz)
 		copy(n, bn.content)
@@ -172,31 +176,40 @@ func (bn *Client) resize(sz uint64) {
 }
 
 // Implement GetAttr to provide size and mtime
-var _ = (fs.NodeGetattrer)((*Client)(nil))
+var _ = (fs.NodeGetattrer)((*FSNode)(nil))
 
-func (n *Client) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+// Getattr is part of the NodeGetattrer interface.
+func (n *FSNode) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
 	// bn.mu.Lock()
 	// defer bn.mu.Unlock()
 	n.getattr(out)
 	return 0
 }
 
-func (n *Client) getattr(out *fuse.AttrOut) {
+// getattr fills out the AttrOut structure.
+func (n *FSNode) getattr(out *fuse.AttrOut) {
 	out.Size = uint64(n.file.sz)
 	out.Owner = fuse.Owner{Uid: 1000, Gid: 1000}
+
+	// setting last modified time
 	out.SetTimes(nil, &time.Time{}, nil)
 }
 
-var _ = (fs.NodeFsyncer)((*Client)(nil))
+// Implement Fsync to support file sync
+var _ = (fs.NodeFsyncer)((*FSNode)(nil))
 
-func (n *Client) Fsync(ctx context.Context, f fs.FileHandle, flags uint32) syscall.Errno {
+// Fsync is part of the NodeFsyncer interface.
+// Doing nothing as of now, but can be used to sync the file to the server.
+func (n *FSNode) Fsync(ctx context.Context, f fs.FileHandle, flags uint32) syscall.Errno {
 	return 0
 }
 
-// Implement Setattr to support truncation
-var _ = (fs.NodeSetattrer)((*Client)(nil))
+// Implement Setattr to support truncation of file.
+var _ = (fs.NodeSetattrer)((*FSNode)(nil))
 
-func (bn *Client) Setattr(ctx context.Context, fh fs.FileHandle, in *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno {
+// Setattr is part of the NodeSetattrer interface.
+// It is used to truncate the file.
+func (bn *FSNode) Setattr(ctx context.Context, fh fs.FileHandle, in *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno {
 	bn.mu.Lock()
 	defer bn.mu.Unlock()
 
@@ -210,10 +223,12 @@ func (bn *Client) Setattr(ctx context.Context, fh fs.FileHandle, in *fuse.SetAtt
 	return 0
 }
 
-// Implement handleless write.
-var _ = (fs.NodeWriter)((*Client)(nil))
+// To Implement handleless write.
+var _ = (fs.NodeWriter)((*FSNode)(nil))
 
-func (bn *Client) Write(ctx context.Context, fh fs.FileHandle, buf []byte, off int64) (uint32, syscall.Errno) {
+// Write is part of the NodeWriter interface.
+// It writes the content of the file to the server. Offset defines the starting point of the write.
+func (bn *FSNode) Write(ctx context.Context, fh fs.FileHandle, buf []byte, off int64) (uint32, syscall.Errno) {
 	bn.mu.Lock()
 	defer bn.mu.Unlock()
 	fmt.Println("Offset:", off)
@@ -227,9 +242,11 @@ func (bn *Client) Write(ctx context.Context, fh fs.FileHandle, buf []byte, off i
 	return uint32(sz), 0
 }
 
-var _ = (fs.NodeCreater)((*Client)(nil))
+// To Implement Create
+var _ = (fs.NodeCreater)((*FSNode)(nil))
 
-func (n *Client) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (node *fs.Inode, fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+// Create is part of the NodeCreater interface. It is called when a new file is created.
+func (n *FSNode) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (node *fs.Inode, fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
 	fmt.Println("Create:", name)
 	fmt.Println("Flags:", flags)
 	fmt.Println("Mode:", mode)
@@ -247,7 +264,7 @@ func (n *Client) Create(ctx context.Context, name string, flags uint32, mode uin
 	fileType := FILE
 
 	file := File{name: n.file.name + "/" + name, id: uint64(id), fileType: fileType, sz: 0}
-	operations := &Client{file: file}
+	operations := &FSNode{file: file}
 
 	// The NewInode call wraps the `operations` object into an Inode.
 	child := n.NewInode(ctx, operations, stable)
@@ -256,9 +273,11 @@ func (n *Client) Create(ctx context.Context, name string, flags uint32, mode uin
 	return child, nil, 0, 0
 }
 
-var _ = (fs.NodeMkdirer)((*Client)(nil))
+// To Implement Mkdir
+var _ = (fs.NodeMkdirer)((*FSNode)(nil))
 
-func (n *Client) Mkdir(ctx context.Context, name string, mode uint32, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+// Mkdir is part of the NodeMkdirer interface. It is called when a new folder is created.
+func (n *FSNode) Mkdir(ctx context.Context, name string, mode uint32, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
 	fileType := FOLDER
 	id := time.Now().Unix()
 
@@ -271,7 +290,7 @@ func (n *Client) Mkdir(ctx context.Context, name string, mode uint32, out *fuse.
 	}
 
 	file := File{name: n.file.name + "/" + name, id: uint64(id), fileType: fileType, sz: 0}
-	operations := &Client{file: file}
+	operations := &FSNode{file: file}
 
 	// The NewInode call wraps the `operations` object into an Inode.
 	child := n.NewInode(ctx, operations, stable)
@@ -284,7 +303,7 @@ func main() {
 	// This is where we'll mount the FS
 	mntDir := "/home/hari/sfs1"
 	os.Mkdir(mntDir, 0777)
-	root := &Client{file: File{name: "/root", id: 0, fileType: FOLDER}}
+	root := &FSNode{file: File{name: "/root", id: 0, fileType: FOLDER}}
 	server, err := fs.Mount(mntDir, root, &fs.Options{
 		MountOptions: fuse.MountOptions{
 			// Set to true to see how the file system works.
