6be16025902dfa1715c72c7e6a622479487ff70e
Gayatri-Priyadarsini <88318175+Gayatri-Priyadarsini@users.noreply.github.com>
2023-04-21 22:45:30 +0530

    Update master-rpc.go

--- datanode/master-rpc.go
+++ datanode/master-rpc.go
@@ -2,58 +2,63 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/rpc"
+	"time"
 )
 
-func getFileTest(master *rpc.Coordinator, filename string) {
-	args := GetFileArgs_m{
-		FileName:   filename,
-		FolderPath: "/home/test1/Desktop",
-	}
-	reply := GetFileReply_m{}
-	master.Call("DataNode.GetFile_m", &args, &reply)
-	fmt.Println("Args:", args, "reply:", reply)
-}
+// func getFileTest(master *rpc.Coordinator, filename string) {
+// 	args := GetFileArgs_m{
+// 		FileName:   filename,
+// 		FolderPath: "/home/test1/Desktop",
+// 	}
+// 	reply := GetFileReply_m{}
+// 	master.Call("DataNode.GetFile_m", &args, &reply)
+// 	fmt.Println("Args:", args, "reply:", reply)
+// }
 
-func insertFileTest(master *rpc.Coordinator, filename string) {
-	args := InsertFileArgs_m{
-		FileName:   filename,
-		FolderPath: "/home/test1/Desktop",
-		FileSize:   100,
-	}
-	reply := InsertFileReply_m{}
-	master.Call("DataNode.InsertFile_m", &args, &reply)
-	fmt.Println("Args:", args, "reply:", reply)
-}
+// func insertFileTest(master *rpc.Coordinator, filename string) {
+// 	args := InsertFileArgs_m{
+// 		FileName:   filename,
+// 		FolderPath: "/home/test1/Desktop",
+// 		FileSize:   100,
+// 	}
+// 	reply := InsertFileReply_m{}
+// 	master.Call("DataNode.InsertFile_m", &args, &reply)
+// 	fmt.Println("Args:", args, "reply:", reply)
+// }
 
-func deleteFileTest(master *rpc.Coordinator, filename string) {
-	args := DeleteFileArgs_m{
-		FileName:   filename,
-		FolderPath: "/home/test1/Desktop",
-	}
-	reply := DeleteFileReply_m{}
-	master.Call("DataNode.DeleteFile_m", &args, &reply)
-	fmt.Println("Args:", args, "reply:", reply)
-}
+// func deleteFileTest(master *rpc.Coordinator, filename string) {
+// 	args := DeleteFileArgs_m{
+// 		FileName:   filename,
+// 		FolderPath: "/home/test1/Desktop",
+// 	}
+// 	reply := DeleteFileReply_m{}
+// 	master.Call("DataNode.DeleteFile_m", &args, &reply)
+// 	fmt.Println("Args:", args, "reply:", reply)
+// }
 
-func (c *Coordinator) InsertFile_done() {
-	//update database about insert file at all datanodes specified
-}
+// func (c *Coordinator) InsertFile_done() {
+// 	//update database about insert file at all datanodes specified
+// }
 
-func (c *Coordinator) DeleteFile_done() {
-	//update database about delete file at all datanodes specified
-}
+// func main() {
+// 	//client, err := rpc.DialHTTP("tcp", "localhost:9000")
+// 	// if err != nil {
+// 	// 	log.Println("Error in dialing rpc:", err)
+// 	// }
+// 	// insertFileTest(master, "test1.txt")
+// 	// insertFileTest(master, "test2.txt")
+// 	// getFileTest(master, "test1.txt")
+// 	// getFileTest(master, "test2.txt")
+// 	// deleteFileTest(master, "test1.txt")
+// 	InitServer()
+// }
 
 func main() {
-	master, err := rpc.DialHTTP("tcp", "localhost:9000")
-	if err != nil {
-		log.Println("Error in dialing rpc:", err)
+	c := InitServer()
+	fmt.Println("Server has started")
+	fmt.Printf("%v\n", c.DataDirectory)
+	for {
+		c.Ping()
+		time.Sleep(time.Second * 30)
 	}
-	insertFileTest(master, "test1.txt")
-	insertFileTest(master, "test2.txt")
-	getFileTest(master, "test1.txt")
-	getFileTest(master, "test2.txt")
-	deleteFileTest(master, "test1.txt")
-
 }
