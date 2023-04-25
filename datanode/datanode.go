846521f14ce6738b8914ddd800e51a237db0b2a3
Gayatri-Priyadarsini <88318175+Gayatri-Priyadarsini@users.noreply.github.com>
2023-04-25 21:03:49 +0530

    Update datanode.go

--- datanode/datanode.go
+++ datanode/datanode.go
@@ -7,8 +7,9 @@ import (
 	"net/http"
 	"net/rpc"
 	"os"
+	"strconv"
 
-	"gopkg.in/ini.v1"
+	//"gopkg.in/ini.v1"
 )
 
 type DataNode struct {
@@ -33,17 +34,20 @@ func (d *DataNode) server(host string, port int) {
 }
 
 func InitServer() *DataNode {
-	cfg, err := ini.Load(".ini")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error in parsing .ini file\n")
-		os.Exit(1)
-	}
-	var host string = cfg.Section("rpc").Key("host").String()
-	var port int = cfg.Section("rpc").Key("port").MustInt()
-	fmt.Println("RPC Conenction", host, port)
+	//cfg, err := ini.Load(".ini")
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Error in parsing .ini file\n")
+	// 	os.Exit(1)
+	// }
+	//var host string = cfg.Section("rpc").Key("host").String()
+	//var port int = cfg.Section("rpc").Key("port").MustInt()
+	//fmt.Println("RPC Conenction", host, port)
 	c := DataNode{}
 	//c.Ping()
-
+	var host string=os.Args[1]
+	var port int
+	port,_=strconv.Atoi(os.Args[2])
+	//fmt.Println(host,port)
 	c.server(host, port)
 	return &c
 }
